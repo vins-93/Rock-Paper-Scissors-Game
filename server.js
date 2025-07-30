4a64be91f07bbd235fce513dd0abc4d4368e9932
vins-93 <vinayakmannikeri93@gmail.com>
2025-07-30 21:35:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
